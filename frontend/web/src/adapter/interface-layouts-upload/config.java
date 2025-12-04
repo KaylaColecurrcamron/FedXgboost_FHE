920545ee2751c0954ac683df4e4f0bc6377675f7
KaylaColecurrcamron <LasureMuralles5647@outlook.com>
2025-12-04 23:48:48 +0800

    add router-module-advanced.py, hardhat.d.ts, config.java

---
+++ frontend/web/src/adapter/interface-layouts-upload/config.java
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\adapter\interface-layouts-upload\config.java
+// Generated at: 2025-10-16T15:23:15.607Z
---
+++ router-module-advanced.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\router-module-advanced.py
+// Generated at: 2025-10-16T15:23:15.591Z
